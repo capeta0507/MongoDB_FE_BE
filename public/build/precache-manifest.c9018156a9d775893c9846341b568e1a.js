3914296a6e1e4ac2191648c5eff3c6c585d60a92
capeta0507 <nintendof1@gmail.com>
2019-08-18 22:49:26 +0800

    React Build APP

---
+++ public/build/precache-manifest.c9018156a9d775893c9846341b568e1a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b71f6c98846e6637e150e97d75481e6e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9c9bee73027a4ea5d7ac",
+    "url": "./static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "de47ec4e7e231a1798c4",
+    "url": "./static/js/2.9b088aff.chunk.js"
+  },
+  {
+    "revision": "9c9bee73027a4ea5d7ac",
+    "url": "./static/js/main.c450dc43.chunk.js"
+  },
+  {
+    "revision": "b7005fa5e5d8e9d2e8e2",
+    "url": "./static/js/runtime~main.eba22b66.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
